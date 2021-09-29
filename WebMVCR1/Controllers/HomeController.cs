cfe802d4b13792f37edcf863a7452481cf6682b4
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-09-29 15:55:21 +0300

    Adds

--- WebMVCR1/Controllers/HomeController.cs
+++ WebMVCR1/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using WebMVCR1.Models;
+using static WebMVCR1.Models.StudyCsharp;
 
 namespace WebMVCR1.Controllers
 {
@@ -11,7 +13,7 @@ namespace WebMVCR1.Controllers
     {
         public string Index(string hel)
         {
-            string res = StudyCsharp.GetFunction(0, 9);
+            string res = ExeCollection();
             return res;
         }
         public string ExeEnum()
@@ -37,7 +39,56 @@ namespace WebMVCR1.Controllers
             string res = String.Format("Информация о банковском счете: {0}", goldBankAccount);
             return res;
         }
+        public string ExeFactorial(int x)
+        {
+            int f;
+            bool ok = StudyCsharp.Factorial(x, out f);
+            if (ok)
+                return String.Format("Факториал числа {0} равен {1} ", x, f);
+            else
+                return "Невозможно вычислить факториал";
+        }
+        public string ExeTriangle()
+        {
+            Triangle tr1 = new Triangle(3, 5, 6);
+            string sq1 = String.Format("Площадь фигуры {0} равна: {1:0.##}",
+                tr1.Name, tr1.Area);
+            return sq1;
+        }
+        public string ExeCircle()
+        {
+            Circle cir1 = new Circle(3);
+            string sq = String.Format("Площадь фигуры {0} равна: {1:0.##}",
+            cir1.Name, cir1.Area);
+            return sq;
+        }
+        public string ExePolim()
+        {
+            StringBuilder str = new StringBuilder();
+            Shape[] sh = { new Triangle(1, 2, 3), new Circle(5), new Triangle(5, 6, 8) };
+            foreach (Shape item in sh) { str.AppendFormat("Это фигура {0}", item.Name + "<p>"); }
+            return str.ToString();
 
+        }
+        public string ExeCollection()
+        {
+            List<Circle> cirs = new List<Circle>
+            {
+                new Circle(12),
+                new Circle(5),
+                new Circle(15),
+                new Circle(6)
+            };
+            cirs.Add(new Circle(7));
+            cirs.Sort();
+            StringBuilder str = new StringBuilder();
+            foreach (Shape item in cirs)
+            {
+                str.AppendFormat("Это фигура {0}", item.Name + "<p>");
+            }
+            return str.ToString();
+
+        }
     }
 
 
--- WebMVCR1/Models/StudyCsharp.cs
+++ WebMVCR1/Models/StudyCsharp.cs
@@ -93,5 +93,53 @@ namespace WebMVCR1.Models
             answer = f;
             return ok;
         }
+        public class Triangle : Shape
+        {
+            
+            public double Stb { get; set; }
+            public double Stc { get; set; }
+            public Triangle(double a, double b, double c)
+            {
+                St = a; Stb = b; Stc = c;
+            }
+            override public string Name => String.Format("\"Треугольник со сторонами {0}, {1} и {2}\"", St, Stb, Stc);
+            public double Perimeter => Math.Round(St + Stb + Stc);
+            public double Area
+            {
+                get
+                {
+                    double sq = Math.Sqrt(Perimeter / 2 * (Perimeter / 2 - St)
+                        * (Perimeter / 2 - Stb) * (Perimeter / 2 - Stc));
+                    return sq;
+                }
+            }
+        }
+        public class Circle : Shape, IComparable<Circle>
+        {
+               
+        public Circle(double a) { St = a; }
+            public double Dlina { get { double p = 2 * Math.PI * St; return p; } }
+            public double Area { get { double sq = Math.PI * St * St; return sq; } }
+            override public string Name
+            {
+                get
+                { return String.Format("\"Окружность с радиусом {0}\"", St); }
+            }
+            public int CompareTo(Circle other)
+            {
+                if (this.Area == other.Area)
+                    return 0;
+                else if (this.Area > other.Area)
+                    return 1;
+                else return -1;
+            }
+        }
     }
-}
+
+        public class Shape
+        {
+            public double St { get; set; }
+            virtual public string Name { get { return String.Format("\"Фигура\""); } }
+        }
+
+    }
