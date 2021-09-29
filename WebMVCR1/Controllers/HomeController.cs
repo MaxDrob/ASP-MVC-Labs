9bc66064d7c3f0f88876ac36f52d834c891af0fc
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-09-29 15:33:37 +0300

    Adds

--- WebMVCR1/Controllers/HomeController.cs
+++ WebMVCR1/Controllers/HomeController.cs
@@ -11,9 +11,35 @@ namespace WebMVCR1.Controllers
     {
         public string Index(string hel)
         {
-            string Greeting = ModelClass.ModelHello() + ", " + hel;
-            return Greeting;
+            string res = StudyCsharp.GetFunction(0, 9);
+            return res;
         }
+        public string ExeEnum()
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+            string res1 = String.Format("Тип банковского счета {0}",
+                goldAccount);
+            string res2 = String.Format("Тип банковского счета {0}",
+                platinumAccount);
+            string res = res1 + "<p>" + res2;
+            return res;
+
+        }
+        public string ExeStruct()
+        {
+            BankAccount goldBankAccount;
+            goldBankAccount.accType = AccountType.Checking;
+            goldBankAccount.accBal = (decimal)3200.00;
+            goldBankAccount.accNo = 123;
+            string res = String.Format("Информация о банковском счете: {0}", goldBankAccount);
+            return res;
+        }
+
     }
-    
+
+
+
 }
