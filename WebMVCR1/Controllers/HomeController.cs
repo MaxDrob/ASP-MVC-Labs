b83a44787680ebd2713b5efb96bb19efc0d415a3
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-09-29 16:23:12 +0300

    Fin Lab 1-3

--- WebMVCR1/Controllers/HomeController.cs
+++ WebMVCR1/Controllers/HomeController.cs
@@ -11,10 +11,36 @@ namespace WebMVCR1.Controllers
 {
     public class MyController : Controller
     {
-        public string Index(string hel)
+        private static PersonRepository db = new PersonRepository();
+        public ViewResult Index()
+
         {
-            string res = ExeCollection();
-            return res;
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            ViewData["Mes"] = "хорошего настроения";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult InputData()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult InputData(Person p)
+        {
+            db.AddResponse(p);
+            return View("Hello", p);
+        }
+        public ViewResult Hello()
+        {
+            return View();
+        }
+        public ViewResult OutputData()
+        {
+            ViewBag.Pers = db.GetAllResponses;
+            ViewBag.Count = db.NumberOfPerson;
+            return View("ListPerson");
         }
         public string ExeEnum()
         {
